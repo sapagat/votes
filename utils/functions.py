8136a37ffcc3207674f6275b52c5f7d10e762e9b
Sam Gathercole <sam@nextail.co>
2019-04-07 21:37:36 +0200

    Add gender classification with a SVM

--- utils/functions.py
+++ utils/functions.py
@@ -16,6 +16,8 @@ from nltk import classify
 from nltk import NaiveBayesClassifier
 import os
 import string
+import numpy as np
+from sklearn import svm
 
 ROOT_PATH = os.getenv('PYTHONPATH')
 DATASET_FOLDER = os.getenv('DATASET_FOLDER') or 'datasets/'
@@ -127,4 +129,38 @@ def classify_with_naive_bayes(data, test_size, describe=True, show_top=10):
     return {
         'accuracy': accuracy,
         'classifier': classifier
+    }
+
+def bag_of_words_for_svm(examples):
+    vocabulary = vocabulary_from(examples)
+    sentences = np.zeros((len(examples), len(vocabulary)))
+    for sentence_id, words in enumerate(examples):
+        for word in words:
+            word_id = vocabulary.index(word)
+            sentences[sentence_id, word_id] += 1
+    return {
+        'vocabulary_size': len(vocabulary),
+        'sentences': sentences
+    }
+
+def vocabulary_from(sentences):
+    vocabulary = set()
+    for sentence in sentences:
+        for word in sentence:
+            if word in vocabulary:
+                continue
+
+            vocabulary.add(word)
+
+    return list(vocabulary)
+
+def classify_with_svm(X, y, test_size, maximum_iterations):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+    classifier = svm.SVC(gamma='scale', max_iter=maximum_iterations)
+    classifier.fit(X_train, y_train)
+    y_predicted = classifier.predict(X_test)
+    total_correct = np.sum(y_predicted == y_test)
+    return {
+        'total_correct': total_correct,
+        'accuracy': float(total_correct) / len(y_test)
     }
