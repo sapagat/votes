d75c31df8e5671361fa2e9b6f129487b832643bd
Sam Gathercole <sam@nextail.co>
2019-04-14 00:18:52 +0200

    Extract svm classifier to classifiers module

--- utils/classifiers.py
+++ utils/classifiers.py
@@ -1,6 +1,8 @@
 from sklearn.model_selection import train_test_split
 from  nltk import classify
 from nltk import NaiveBayesClassifier
+import numpy as np
+from sklearn import svm
 
 def classify_with_naive_bayes(data, test_size):
     training, test = train_test_split(
@@ -14,6 +16,49 @@ def classify_with_naive_bayes(data, test_size):
     return {
         'accuracy': accuracy,
         'classifier': classifier
+    }
+
+def bag_of_words_for_svm(examples):
+    vocabulary = vocabulary_from(examples)
+    sentences = np.zeros((len(examples), len(vocabulary)))
+    for sentence_id, words in enumerate(examples):
+        for word in words:
+            word_id = vocabulary.index(word)
+            sentences[sentence_id, word_id] += 1
+    return {
+        'vocabulary_size': len(vocabulary),
+        'sentences': sentences
+    }
+
+def vocabulary_from(sentences):
+    vocabulary = set()
+    for sentence in sentences:
+        for word in sentence:
+            if word in vocabulary:
+                continue
+
+            vocabulary.add(word)
+
+    return list(vocabulary)
+
+def classify_with_svm(X, y, test_size, maximum_iterations):
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=test_size,
+        random_state=random_state()
+    )
+    classifier = svm.SVC(
+        gamma='scale',
+        max_iter=maximum_iterations,
+        random_state=random_state()
+    )
+    classifier.fit(X_train, y_train)
+    y_predicted = classifier.predict(X_test)
+    total_correct = np.sum(y_predicted == y_test)
+    return {
+        'total_correct': total_correct,
+        'accuracy': float(total_correct) / len(y_test)
     }
 
 def random_state():
