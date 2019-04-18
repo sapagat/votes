bcac29ba08e492749dac8ffbdfa7163645aece11
MartaHdz <marta.hdezr@gmail.com>
2019-04-18 20:29:14 +0200

    GenderRandomSubsampleExploration

--- utils/frame.py
+++ utils/frame.py
@@ -14,3 +14,28 @@ def counts_by_category(tweets):
 
 def plot_categories_distribution(tweets):
     sns.distplot(counts_by_category(tweets))
+
+def random_subsample(tweets, fraction):
+    return tweets.sample(frac = fraction)
+
+def random_subsamples(tweets, fraction, replicas):
+    result = []
+    for i in range(replicas):
+        result.append(percentage_by_category(random_subsample(tweets, fraction)).to_dict())
+    return result
+
+def grid_subsamples(tweets, fractions, replicas):
+    descriptor = {
+        'categories': tweets['category'].unique(),
+        'subsamples': []
+    }
+    for fraction in fractions:
+        descriptor['subsamples'].append({
+            'fraction': fraction,
+            'replicas': random_subsamples(tweets, fraction, replicas)
+        })
+    return descriptor
+
+def percentage_by_category(tweets):
+    total = tweets.shape[0]
+    return counts_by_category(tweets) / total
